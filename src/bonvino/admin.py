9cacda5279df45c52a48be16d814bd24a68f79a8
martin gaido <martingaido00@gmail.com>
2024-06-20 18:00:19 -0300

    queda arreglar unas cosas

--- src/bonvino/admin.py
+++ src/bonvino/admin.py
@@ -1,97 +1,71 @@
 from django.contrib import admin
-from bonvino.models import *
+from django.contrib.auth.admin import UserAdmin
+from .models import RegionVitivinicola, Provincia, Pais, Maridaje, NovedadEvento, Bodega, Vino, Varietal, TipoUva, Reseña, Enofilo, Sommelier, Certificacion, CobroPremium, Siguiendo
+
+# Register your models here.
 
 @admin.register(RegionVitivinicola)
 class RegionVitivinicolaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'descripcion')
-    search_fields = ('nombre', 'descripcion')
+    list_display = ('id', 'nombre', 'descripcion')
 
 @admin.register(Provincia)
 class ProvinciaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'region_vitivinicola')
-    search_fields = ('nombre',)
-    list_filter = ('region_vitivinicola',)
+    list_display = ('id', 'nombre', 'region_vitivinicola')
 
 @admin.register(Pais)
 class PaisAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    search_fields = ('nombre',)
-    filter_horizontal = ('provincias',)
+    list_display = ('id', 'nombre')
 
 @admin.register(Maridaje)
 class MaridajeAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'descripcion')
-    search_fields = ('nombre', 'descripcion')
+    list_display = ('id', 'nombre', 'descripcion')
 
 @admin.register(NovedadEvento)
 class NovedadEventoAdmin(admin.ModelAdmin):
-    list_display = ('nombreEvento', 'fechaHoraEvento', 'esSoloPremium')
-    search_fields = ('nombreEvento', 'descripcion')
-    list_filter = ('esSoloPremium', 'fechaHoraEvento')
+    list_display = ('id', 'nombreEvento', 'descripcion', 'fechaHoraEvento', 'esSoloPremium', 'codigoDescuentoPremium')
 
 @admin.register(Bodega)
 class BodegaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'region_vitivinicola', 'periodoActualizacion')
-    search_fields = ('nombre', 'descripcion', 'historia')
-    list_filter = ('region_vitivinicola', 'periodoActualizacion')
+    list_display = ('id', 'nombre', 'region_vitivinicola', 'coordenadasUbicacion', 'periodoActualizacion')
 
 @admin.register(Vino)
 class VinoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'añada', 'bodega', 'precioARS', 'fechaActualizacion')
-    search_fields = ('nombre', 'añada', 'notaDeCataBodega')
-    list_filter = ('bodega', 'fechaActualizacion')
+    list_display = ('id', 'nombre', 'añada', 'precioARS', 'bodega', 'fechaActualizacion')
     filter_horizontal = ('maridajes',)
 
 @admin.register(Varietal)
 class VarietalAdmin(admin.ModelAdmin):
-    list_display = ('tipo_uva', 'vino', 'porcentajeComposicion')
-    search_fields = ('descripcion',)
-    list_filter = ('tipo_uva', 'vino')
+    list_display = ('id', 'vino', 'descripcion', 'porcentajeComposicion', 'tipo_uva')
 
 @admin.register(TipoUva)
 class TipoUvaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'descripcion')
-    search_fields = ('nombre', 'descripcion')
+    list_display = ('id', 'nombre', 'descripcion')
 
 @admin.register(Reseña)
 class ReseñaAdmin(admin.ModelAdmin):
-    list_display = ('vino', 'usuario', 'puntaje', 'fechaReseña', 'esPremium')
-    search_fields = ('comentario',)
-    list_filter = ('vino', 'usuario', 'esPremium', 'fechaReseña')
-
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'premium')
-    search_fields = ('nombre',)
-    list_filter = ('premium',)
+    list_display = ('id', 'vino', 'usuario', 'fechaReseña', 'puntaje', 'esPremium')
 
 @admin.register(Enofilo)
-class EnofiloAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'apellido', 'premium')
-    search_fields = ('nombre', 'apellido')
-    list_filter = ('premium',)
-    filter_horizontal = ('amigos', 'favoritos')
+class EnofiloAdmin(UserAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'apellido', 'get_premium')  # Agregamos get_premium como método
+    filter_horizontal = ('favoritos',)  # Eliminamos amigos aquí debido a la restricción
+
+    def get_premium(self, obj):
+        return obj.premium
+    get_premium.short_description = 'premium'
 
 @admin.register(Sommelier)
-class SommelierAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'fechaValidacion', 'premium')
-    search_fields = ('nombre', 'notaPresentacion')
-    list_filter = ('fechaValidacion', 'premium')
-    filter_horizontal = ('certificaciones',)
+class SommelierAdmin(UserAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'fechaValidacion', 'notaPresentacion')
 
 @admin.register(Certificacion)
 class CertificacionAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'institucionOtorgante', 'fechaInicio', 'fechaFin')
-    search_fields = ('nombre', 'institucionOtorgante', 'descripcion')
-    list_filter = ('institucionOtorgante', 'fechaInicio', 'fechaFin')
+    list_display = ('id', 'nombre', 'institucionOtorgante', 'fechaInicio', 'fechaFin')
 
 @admin.register(CobroPremium)
 class CobroPremiumAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'monto', 'fechaPago', 'esAnual')
-    search_fields = ('nroOperacionMercadoPago',)
-    list_filter = ('esAnual', 'fechaPago')
+    list_display = ('id', 'usuario', 'fechaPago', 'monto', 'esAnual', 'nroOperacionMercadoPago')
 
 @admin.register(Siguiendo)
 class SiguiendoAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'amigo', 'bodega', 'sommelier', 'fechaInicio', 'fechaFin')
-    list_filter = ('usuario', 'amigo', 'bodega', 'sommelier', 'fechaInicio', 'fechaFin')
+    list_display = ('id', 'usuario', 'amigo', 'bodega', 'sommelier', 'fechaInicio', 'fechaFin')
--- src/bonvino/models.py
+++ src/bonvino/models.py
@@ -1,20 +1,33 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 class RegionVitivinicola(models.Model):
     descripcion = models.CharField(max_length=255)
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
+
 class Provincia(models.Model):
     nombre = models.CharField(max_length=255)
-    region_vitivinicola = models.ForeignKey(RegionVitivinicola, on_delete=models.CASCADE, related_name='provincias')
+    regiones_vitivinicolas = models.ManyToManyField(RegionVitivinicola, related_name='provincias')
+
+    def __str__(self):
+        return self.nombre
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=255)
     provincias = models.ManyToManyField(Provincia, related_name='paises')
 
+    def __str__(self):
+        return self.nombre
 class Maridaje(models.Model):
     descripcion = models.CharField(max_length=255)
     nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
 
 class NovedadEvento(models.Model):
     codigoDescuentoPremium = models.CharField(max_length=255)
@@ -22,6 +35,9 @@ class NovedadEvento(models.Model):
     esSoloPremium = models.BooleanField()
     fechaHoraEvento = models.DateTimeField()
     nombreEvento = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombreEvento
 
 class Bodega(models.Model):
     coordenadasUbicacion = models.CharField(max_length=255)
@@ -31,15 +47,20 @@ class Bodega(models.Model):
     periodoActualizacion = models.DateField()
     region_vitivinicola = models.ForeignKey(RegionVitivinicola, on_delete=models.CASCADE, related_name='bodegas')
 
+    def __str__(self):
+        return self.nombre
+
 class Vino(models.Model):
     añada = models.CharField(max_length=255)
     fechaActualizacion = models.DateField()
-    imagenEtiqueta = models.ImageField(upload_to='vinos/')
     nombre = models.CharField(max_length=255)
     notaDeCataBodega = models.TextField()
     precioARS = models.DecimalField(max_digits=10, decimal_places=2)
     bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE, related_name='vinos')
     maridajes = models.ManyToManyField(Maridaje, related_name='vinos')
+
+    def __str__(self):
+        return self.nombre
 
 class Varietal(models.Model):
     descripcion = models.CharField(max_length=255)
@@ -47,9 +68,15 @@ class Varietal(models.Model):
     vino = models.ForeignKey(Vino, on_delete=models.CASCADE, related_name='varietales')
     tipo_uva = models.ForeignKey('TipoUva', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.descripcion
+
 class TipoUva(models.Model):
     descripcion = models.CharField(max_length=255)
     nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
 
 class Reseña(models.Model):
     comentario = models.TextField()
@@ -57,23 +84,27 @@ class Reseña(models.Model):
     fechaReseña = models.DateField()
     puntaje = models.IntegerField()
     vino = models.ForeignKey(Vino, on_delete=models.CASCADE, related_name='reseñas')
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE, related_name='reseñas')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reseñas')
 
-class Usuario(models.Model):
-    contraseña = models.CharField(max_length=255)
-    nombre = models.CharField(max_length=255)
-    premium = models.BooleanField()
+    def __str__(self):
+        return f'Reseña de {self.vino.nombre} por {self.usuario.username}'
 
-class Enofilo(Usuario):
+class Enofilo(User):
     apellido = models.CharField(max_length=255)
-    imagenPerfil = models.ImageField(upload_to='perfiles/')
+    imagenPerfil = models.CharField(max_length=255, blank=True)
     amigos = models.ManyToManyField('self', through='Siguiendo', symmetrical=False, related_name='seguidores')
     favoritos = models.ManyToManyField(Vino, related_name='enofilos_favoritos')
 
-class Sommelier(Usuario):
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Sommelier(User):
     fechaValidacion = models.DateField()
     notaPresentacion = models.TextField()
     certificaciones = models.ManyToManyField('Certificacion', related_name='sommeliers')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Certificacion(models.Model):
     adjuntoURL = models.URLField()
@@ -83,17 +114,26 @@ class Certificacion(models.Model):
     institucionOtorgante = models.CharField(max_length=255)
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
+
 class CobroPremium(models.Model):
     esAnual = models.BooleanField()
     fechaPago = models.DateField()
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     nroOperacionMercadoPago = models.CharField(max_length=255)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='cobros_premium')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cobros_premium')
+
+    def __str__(self):
+        return f'Cobro de Premium para {self.usuario.username}'
 
 class Siguiendo(models.Model):
     fechaFin = models.DateField()
     fechaInicio = models.DateField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='siguiendo')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='siguiendo')
     amigo = models.ForeignKey(Enofilo, on_delete=models.CASCADE, related_name='seguido_por', null=True, blank=True)
     bodega = models.ForeignKey(Bodega, on_delete=models.CASCADE, related_name='seguido_por', null=True, blank=True)
     sommelier = models.ForeignKey(Sommelier, on_delete=models.CASCADE, related_name='seguido_por', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.usuario.username} sigue a {self.amigo or self.bodega or self.sommelier}'
